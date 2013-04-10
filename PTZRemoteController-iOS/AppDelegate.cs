d4aedb7b89dfcbe2e773a13280fba0abbcf557e0
Greg Shackles <gshackles@gmail.com>
2013-04-10 01:30:28 -0400

    adding a basic UI for the iOS app

--- PTZRemoteController-iOS/AppDelegate.cs
+++ PTZRemoteController-iOS/AppDelegate.cs
@@ -1,39 +1,35 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-
 using MonoTouch.Foundation;
 using MonoTouch.UIKit;
 
 namespace PTZRemoteControlleriOS
 {
-	// The UIApplicationDelegate for the application. This class is responsible for launching the 
-	// User Interface of the application, as well as listening (and optionally responding) to 
-	// application events from iOS.
 	[Register ("AppDelegate")]
 	public partial class AppDelegate : UIApplicationDelegate
 	{
-		// class-level declarations
-		UIWindow window;
-		RemoteViewController viewController;
+		private UIWindow _window;
+		private UIViewController _rootViewController;
 
-		//
-		// This method is invoked when the application has loaded and is ready to run. In this 
-		// method you should instantiate the window, load the UI into it and then make the window
-		// visible.
-		//
-		// You have 17 seconds to return from this method, or iOS will terminate your application.
-		//
 		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
 		{
-			window = new UIWindow (UIScreen.MainScreen.Bounds);
+			_window = new UIWindow (UIScreen.MainScreen.Bounds);
 			
-			viewController = new RemoteViewController ();
-			window.RootViewController = viewController;
-			window.MakeKeyAndVisible ();
+            var prefs = NSUserDefaults.StandardUserDefaults;
+
+            if (prefs.StringForKey("RelayServerUrl") == null)
+            {
+                prefs.SetString("https://hanselmanlyncrelay.azurewebsites.net", "RelayServerUrl");
+                prefs.SetString("SHANSELMAN", "RemoteGroup");
+                prefs.SetString("RelayHub", "HubName");
+            }
+
+            prefs.Init();
+
+            _rootViewController = new UINavigationController(new RemoteView());
+
+			_window.RootViewController = _rootViewController;
+			_window.MakeKeyAndVisible ();
 			
 			return true;
 		}
 	}
 }
-
---
+++ PTZRemoteController-iOS/RemoteView.cs
@@ -0,0 +1,118 @@
+using System;
+using System.Threading.Tasks;
+using MBProgressHUD;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+using PTZRemoteController.Core;
+
+namespace PTZRemoteControlleriOS
+{
+    public partial class RemoteView : UIViewController
+    {
+        private PTZRemote _remote;
+        private double _currentZoomLevel = 0;
+
+        public RemoteView() : base ("RemoteView", null)
+        {
+        }
+		
+        public override async void ViewDidLoad ()
+        {
+            base.ViewDidLoad();
+
+            Title = "PTZ Remote";
+            View.BackgroundColor = UIColor.LightGray;
+
+            if (await ConnectToRelay())
+                AttachHandlers();
+
+            var refreshButton = new UIBarButtonItem(UIBarButtonSystemItem.Refresh);
+            refreshButton.Clicked += delegate { ConnectToRelay(); };
+            NavigationItem.RightBarButtonItem = refreshButton;
+        }
+
+        private async Task<bool> ConnectToRelay()
+        {
+            bool connected = false;
+
+            var waitIndicator = new MTMBProgressHUD (View) 
+            {
+                LabelText = "Connecting...",
+                DimBackground = true,
+                AnimationType = MBProgressHUDAnimation.MBProgressHUDAnimationZoomIn,
+                Mode = MBProgressHUDMode.Indeterminate,
+                MinShowTime = 0,
+                RemoveFromSuperViewOnHide = true
+            };
+            View.AddSubview(waitIndicator);
+            waitIndicator.Show(animated: true);
+
+            try
+            {
+                var prefs = NSUserDefaults.StandardUserDefaults;
+                
+                _remote = new PTZRemote(
+                    prefs.StringForKey("RelayServerUrl"), 
+                    prefs.StringForKey("RemoteGroup"), 
+                    prefs.StringForKey("HubName"));
+                connected = await _remote.Connect();
+            }
+            catch (Exception)
+            {
+            }
+            finally
+            {
+                waitIndicator.Hide(animated: true);
+            }
+
+            ShowMessage(connected ? "Connected!" : "Unable to connect");
+
+            return connected;
+        }
+
+        private void AttachHandlers()
+        {
+            MoveUp.TouchUpInside += delegate { _remote.MoveUp(); };
+            MoveDown.TouchUpInside += delegate { _remote.MoveDown(); };
+            MoveRight.TouchUpInside += delegate { _remote.MoveRight(); };
+            MoveLeft.TouchUpInside += delegate { _remote.MoveLeft(); };
+            
+            ZoomLevel.MinimumValue = double.MinValue;
+            ZoomLevel.MaximumValue = double.MaxValue;
+            ZoomLevel.Value = _currentZoomLevel;
+            ZoomLevel.ValueChanged += delegate 
+            {
+                if (ZoomLevel.Value > _currentZoomLevel)
+                    _remote.ZoomIn();
+                else
+                    _remote.ZoomOut();
+                
+                _currentZoomLevel = ZoomLevel.Value;
+            };
+        }
+
+        private void ShowMessage(string message) 
+        {
+            var hud = new MTMBProgressHUD (View) 
+            {
+                DetailsLabelText = message,
+                RemoveFromSuperViewOnHide = true,
+                DimBackground = false,
+                AnimationType = MBProgressHUDAnimation.MBProgressHUDAnimationZoomIn,
+                Mode = MBProgressHUDMode.Text
+            };
+            View.AddSubview (hud);
+
+            hud.Show (animated: true);
+            hud.Hide (animated: true, delay: 1.5);
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            
+            ReleaseDesignerOutlets();
+        }
+    }
+}
+
